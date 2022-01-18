17e5a6c99f914a2e16da39ef892fd68ae4c9dfc5
Havish123 <havishnadar@gmail.com>
2022-01-18 21:36:41 +0530

    Initial commit

---
+++ app/src/main/java/com/havish/gce_tvl/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.havish.gce_tvl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
